c5cde3f834c283543513e2886288b79d9fe23a30
chante81 <chanteosborne@gmail.com>
2024-06-25 16:42:11 -0400

    adding employee controller, service

---
+++ Services/EmployeeService.cs
@@ -0,0 +1,15 @@
+using Capstone.Data;
+
+namespace Capstone.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
