27f61f2384f0c558c74e867321211b96997d8784
Parsonsda <david.a.parsons3@gmail.com>
2024-06-27 07:51:05 -0400

    Updates to Login Controller, Services, and DTO

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,3 +1,5 @@
+using Capstone.DTOs;
+using Capstone.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Capstone.Controllers
@@ -6,6 +8,59 @@ namespace Capstone.Controllers
     [Route("[controller]")]
     public class LoginController : ControllerBase
     {
+        private readonly ILoginService _loginService;
+        public LoginController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
 
+        [HttpGet]
+        public ActionResult<IEnumerable<LoginDTO>> GetAllLogins()
+        {
+            var login = _loginService.GetAllLogins();
+            return Ok(login);
+        }
+
+        [HttpGet("{LoginId}")]
+        public ActionResult<LoginDTO> GetLoginById(int LoginId)
+        {
+            var login = _loginService.GetLoginById(LoginId);
+            return login;
+        }
+
+        [HttpPost]
+        public ActionResult<LoginDTO> PostLogin(LoginDTO loginDto)
+        {
+            var login = _loginService.CreateLogin(loginDto);
+            return CreatedAtAction(nameof(GetLoginById), new { LoginId = login.LoginId}, loginDto);
+        }
+
+        [HttpPut("{LoginId}")]
+        public ActionResult<LoginDTO> UpdateLogin(int LoginId, LoginDTO updatedLogin)
+        {
+            _loginService.UpdateLogin(LoginId, updatedLogin);
+            return Ok(updatedLogin);
+        }
+
+        [HttpDelete("{LoginId}")]
+        public IActionResult DeleteLogin(int LoginId)
+        {
+            _loginService.DeleteLogin(LoginId);
+            return Ok();
+        }
+
+        // POST: /Users/login
+        // Logs in a user
+        [HttpPost("login")]
+        public async Task<ActionResult<LoginDTO>> Login(LoginDTO userLogin)
+        {
+            var login = await _loginService.LoginUser(userLogin);
+
+            if (login == null)
+            {
+                return Unauthorized();
+            }
+            return login;
+        }
     }
 }
--- Services/LoginService.cs
+++ Services/LoginService.cs
@@ -1,4 +1,7 @@
 using Capstone.Data;
+using Capstone.DTOs;
+using Capstone.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Capstone.Services
 {
@@ -11,5 +14,34 @@ namespace Capstone.Services
             _context = context;
         }
 
+        public Login CreateLogin(LoginDTO loginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteLogin(int LoginId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<LoginDTO> GetAllLogins()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LoginDTO GetLoginById(int LoginId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult<LoginDTO>> LoginUser(LoginDTO userLogin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateLogin(int LoginId, LoginDTO updatedLogin)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
