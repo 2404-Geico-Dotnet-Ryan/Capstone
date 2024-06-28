2ca43dfdf64d52b25faeeeb254ea87b38db4cca5
chante81 <chanteosborne@gmail.com>
2024-06-28 15:58:14 -0400

    LandingPage Updates

--- capstone/src/Components/LandingPageComponent/LandingPageComponent.tsx
+++ capstone/src/Components/LandingPageComponent/LandingPageComponent.tsx
@@ -1,68 +1,77 @@
 import React, { useEffect, useState } from 'react'
 import EmployeeComponent from '../EmployeeComponent/EmployeeComponent';
+import AnniversaryComponent from '../EmployeeComponent/AnniversaryComponent';
 // import axios from 'axios';
 
 function LandingPageComponent() {
 
-    /*
-        1. Non static banner - Upcoming Events:
-        2. function to get all current month's birthdays and work anniversaries (set up displays)
-        3. function and displays for current user Happy Birthday or Happy Anniversary (extra-nice to have)
-    */
+  /*
+      1. Non static banner - Upcoming Events:
+      2. function to get all current month's birthdays and work anniversaries (set up displays)
+      3. function and displays for current user Happy Birthday or Happy Anniversary (extra-nice to have)
+  */
 
-//  Set variables to get our current Month//       
-const today = new Date();
-let month =today.getMonth();
-const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-let monthname = months[month]
+  //  Set variables to get our current Month//       
+  const today = new Date();
+  let month = today.getMonth();
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let monthname = months[month]
 
-//This is for getting the current month Birthdays from the DB//
+  //This is for getting the current month Birthdays from the DB//
 
-let [employees, setEmployees] = useState<any[] | undefined>(undefined);
+  let [employees, setEmployees] = useState<any[] | undefined>(undefined);
 
-useEffect(() => {
-  async function getEmployees(){
+  useEffect(() => {
+    async function getEmployees() {
       // try{
-          let response = await fetch('http://localhost:5074/Employee');
-          let data = await response.json();
-          setEmployees(data.employees);
-          console.log(data);
+      let response = await fetch('http://localhost:5074/Employee');
+      let data = await response.json();
+      setEmployees(data);
+      console.log(data);
       // }catch(error){
       //     console.error(error);
-  // }    
-  }
-  getEmployees();
-}, []);
+      // }    
+    }
+    getEmployees();
+  }, []);
 
 
 
   return (
     <>
-    <div id="EventHeader">
-    <h1>Upcoming Events...</h1>
-    </div>
+      <div id="EventHeader">
+        <h1>Upcoming Events...</h1>
+      </div>
 
-    <div>
-      <div id="Birthdays">
-        <h2>{monthname} Birthdays</h2>
-        <div>
-        {
-            employees?.map((obj, index) => {
+      <div>
+        <div id="Birthdays">
+          <h2>{monthname} Birthdays</h2>
+          <div>
+            {
+              employees?.map((obj, employeeId) => {
                 return (
-                    <EmployeeComponent employee={obj} key={index}/>
-                );
-            })
-        }
+                  <EmployeeComponent employee={obj} key={employeeId} />
+                )
+              })
+            }
+          </div>
+
+
         </div>
-        
-
+        <div id="Anniversaries">
+          <h2>{monthname} Anniversaries</h2>
+          <div>
+            {
+              employees?.map((obj, employeeId) => {
+                return (
+                  <AnniversaryComponent employee={obj} key={employeeId} />
+                )
+              })
+            }
+          </div>
+        </div>
       </div>
-      <div id="Anniversaries">
-        <h2>{monthname} Anniversaries</h2>
 
-      </div>
-    </div>
-    
     </>
   )
 }
